18b770917ba854b3fbe786944385674ea107b6e3
marinstrba <marinstrba@gmail.com>
2023-12-19 20:14:45 +0100

    I have added frontend functions so user can modify metadata of the video.

--- Frontend/video-streaming-service-ui/src/app/app.module.ts
+++ Frontend/video-streaming-service-ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'; // Standard import
 import { RouterModule } from '@angular/router';
 import { NgxFileDropModule } from 'ngx-file-drop';
@@ -13,6 +13,17 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { SaveVideoDetailsComponent } from './save-video-details/save-video-details.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
+import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgControlsModule} from '@videogular/ngx-videogular/controls';
+import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
+import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
+
 
 @NgModule({
   declarations: [
@@ -31,7 +42,18 @@ import { SaveVideoDetailsComponent } from './save-video-details/save-video-detai
     MatButtonModule,
     BrowserAnimationsModule,
     MatToolbarModule, // Ensure Angular Material modules are imported
-    MatIconModule
+    MatIconModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatChipsModule,
+	VgCoreModule,
+	VgControlsModule,
+	VgOverlayPlayModule,
+	VgBufferingModule
   ],
   bootstrap: [AppComponent]
 })
--- Frontend/video-streaming-service-ui/src/app/save-video-details/save-video-details.component.ts
+++ Frontend/video-streaming-service-ui/src/app/save-video-details/save-video-details.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipsModule } from '@angular/material/chips';
+
+
 
 @Component({
   selector: 'app-save-video-details',
   templateUrl: './save-video-details.component.html',
-  styleUrl: './save-video-details.component.css'
+  styleUrls: ['./save-video-details.component.css'] // Corrected to styleUrls
 })
-export class SaveVideoDetailsComponent {
+export class SaveVideoDetailsComponent implements OnInit {
 
+  saveVideoDetailsForm: FormGroup;
+  title: FormControl = new FormControl('');
+  description: FormControl = new FormControl('');
+  videoStatus: FormControl = new FormControl('');
+	 selectable = true;
+	 removable = true;
+	 addOnBlur = true;
+	 readonly separatorKeysCodes = [ENTER, COMMA] as const;
+	 tags: string[] = [];
+
+
+	   constructor() {
+         this.saveVideoDetailsForm = new FormGroup({
+     		title: this.title,
+     		description: this.description,
+     		videoStatus: this.videoStatus,
+         })
+       }
+
+       ngOnInit(): void {}
+
+	add(event: MatChipInputEvent): void {
+      const input = event.input;
+      const value = (event.value || '').trim();
+
+      if (value) {
+        this.tags.push(value);
+      }
+
+      // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+    }
+
+
+	 remove(value: string): void {
+		const index = this.tags.indexOf(value);
+
+		if (index >= 0) {
+			this.tags.splice(index, 1);
+		}
+  }
 }
